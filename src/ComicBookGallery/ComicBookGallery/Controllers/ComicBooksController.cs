1dc47f232b4f1f68315f0e3e65ca67982c575620
Jamey Wooten <jameywooten@outlook.com>
2019-11-25 08:28:48 -0700

    Added ComicBook detail View

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,12 +11,7 @@ namespace ComicBookGallery.Controllers
         // GET: ComicBooks
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("<h1>Hello from the Details Controller</h1>");
+            return View();
         }
     }
 }

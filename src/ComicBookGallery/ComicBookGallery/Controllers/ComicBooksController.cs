9794de992577226782356ad9f08d696fae8bb8bd
Jamey Wooten <jameywooten@outlook.com>
2019-11-25 15:13:39 -0700

    Added ComicBook Repository

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -12,24 +12,7 @@ namespace ComicBookGallery.Controllers
         // GET: ComicBooks
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing SpiderMan",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the death of Dr. Octopus!</p>",
-                Artist = new Artist[]
-                {
-                    new Artist() {Name = "Dan Slott", Role ="Script"},
-                    new Artist() {Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artist() {Name = "Victor Olazaba", Role = "Inks"},
-                    new Artist() {Name = "Edgar Delgado", Role = "Colors"},
-                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters"},
-                }
-
-
-            };
-
-            return View(comicBook);
+            return View();
         }
     }
 }

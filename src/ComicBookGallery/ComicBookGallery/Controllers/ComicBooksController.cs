812e22e5070dc23f01ee34422b8b8a29dd4a78a4
Jamey Wooten <jameywooten@outlook.com>
2019-11-25 11:23:23 -0700

    Updated ComicBook Detail View to be strongly typed

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery.Models;
 
 namespace ComicBookGallery.Controllers
 {
@@ -11,19 +12,24 @@ namespace ComicBookGallery.Controllers
         // GET: ComicBooks
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing SpiderMan";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the death of Dr. Octopus!</p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos",
+                SeriesTitle = "The Amazing SpiderMan",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the death of Dr. Octopus!</p>",
+                Artist = new Artist[]
+                {
+                    new Artist() {Name = "Dan Slott", Role ="Script"},
+                    new Artist() {Name = "Humberto Ramos", Role = "Pencils"},
+                    new Artist() {Name = "Victor Olazaba", Role = "Inks"},
+                    new Artist() {Name = "Edgar Delgado", Role = "Colors"},
+                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters"},
+                }
+
+
             };
 
-            return View();
+            return View(comicBook);
         }
     }
 }

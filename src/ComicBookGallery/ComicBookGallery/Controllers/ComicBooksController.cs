0a269fec79376db4c5f27223a4230b329f325c78
Jamey Wooten <jameywooten@outlook.com>
2019-11-26 22:23:27 -0700

    Updated ComicBooksController to use Repository

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ComicBookGallery.Models;
+using ComicBookGallery.Data;
 
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        // GET: ComicBooks
-        public ActionResult Detail()
+
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
         {
-            return View();
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        // GET: ComicBooks
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
+
+            return View(comicBook);
         }
     }
 }
